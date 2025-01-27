76f610b8b563f92d99769a4663b6029fedd09341
GamingConsoles <maksiurino@gmail.com>
2025-01-27 07:06:07 +0100

    Added version 1.

---
+++ src/main/java/org/maksiurino/moar_tools/MOARTools.java
@@ -0,0 +1,16 @@
+package org.maksiurino.moar_tools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MOARTools implements ModInitializer {
+	public static final String MOD_ID = "moar_tools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/maksiurino/moar_tools/MOARToolsClient.java
@@ -0,0 +1,11 @@
+package org.maksiurino.moar_tools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MOARToolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
